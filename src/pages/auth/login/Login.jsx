21b1509c0db84cd562655ba17cd2bb79087c3a3b
khouloudhaddad <khouloud.haddad@gmail.com>
2023-04-10 23:29:46 +0100

    Test axios

--- src/pages/auth/login/Login.jsx
+++ src/pages/auth/login/Login.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 export default function Login() {
+
+  const [permissions, setPermissions] = useState([])
+
+    function getPermissions(){
+      axios.get("http://localhost:5000/permissions").then((permissions)=>{
+        console.log(permissions.data)
+        setPermissions(permissions.data)
+      })
+    }
+
+    useEffect(()=>{
+      getPermissions()
+    }, [])
+
   return (
-    <div>Login</div>
+    <div className="permissions">
+    {permissions.map((x) => (
+      <div className="permission" key={x._id}>{x.name}</div>
+    ))
+    }
+  </div>
   )
 }
